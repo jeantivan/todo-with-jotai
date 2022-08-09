d78e08a6ee25e92df0004914c67ddc67705cd6f4
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-09 16:17:46 -0400

    ADD: Estilos globales

---
+++ src/globalStyles.ts
@@ -0,0 +1,23 @@
+import { globalCss } from "@stitches/react";
+
+const globalStyles = globalCss({
+  ":root": {
+    fontFamily: "Inter, Avenir, Helvetica, Arial, sans-serif",
+    fontSynthesis: "none",
+    textRendering: "optimizeLegibility",
+    "-webkit-font-smoothing": "antialiased",
+    "-moz-osx-font-smoothing": "grayscale",
+    "-webkit-text-size-adjust": "100%",
+    color: "#242424",
+    fontSize: 16,
+    lineHeight: "24px",
+    fontWeight: 400,
+  },
+
+  "*": {
+    margin: 0,
+    boxSizing: "border-box",
+  },
+});
+
+export default globalStyles;
