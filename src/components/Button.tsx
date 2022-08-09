193ff891f556b80c406af9f73894231fa4b0fef6
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-09 16:41:03 -0400

    ADD: Botón estilizado con variantes

---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import { styled } from "@stitches/react";
+import { blue, blueA, red, redA, amber, amberA } from "@radix-ui/colors";
+
+const Button = styled("button", {
+  outline: "none",
+  textTransform: "uppercase",
+  fontWeight: 600,
+  color: "white",
+  fontSize: 16,
+  border: 0,
+  padding: "8px 20px",
+
+  variants: {
+    rounded: {
+      full: {
+        borderRadius: 9999,
+      },
+      base: {
+        borderRadius: 8,
+      },
+    },
+    action: {
+      base: {
+        background: `linear-gradient(45deg, ${blue.blue8}, ${blueA.blueA9})`,
+      },
+      danger: {
+        background: `linear-gradient(45deg, ${red.red8}, ${redA.redA9})`,
+      },
+      urgent: {
+        background: `linear-gradient(45deg, ${amber.amber8}, ${amberA.amberA9})`,
+      },
+    },
+  },
+  defaultVariants: {
+    rounded: "base",
+    action: "base",
+  },
+});
+
+export default Button;
