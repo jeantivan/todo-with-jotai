62e923d6d09d0c9afe8c24fd42c484b1ca1a186c
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-09 18:32:42 -0400

    CHANGE: AppContainer y Title en archivos separados

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,11 @@
+import { styled } from "@stitches/react";
+
+const AppContainer = styled("main", {
+  minWidth: "100%",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  padding: "32px 0",
+});
+
+export default AppContainer;
---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@stitches/react";
+
+const Title = styled("h1", {
+  fontSize: "1.5rem",
+  fontWeight: 700,
+  marginBottom: 24,
+  margin: "0 auto",
+});
+
+export default Title;
