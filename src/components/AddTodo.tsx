82bd1519f244ee69255c554b7c45cb1fc5183c6b
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-09 20:43:31 -0400

    FEATURE: Agrega nuevos todos

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,6 +1,11 @@
 import Input from "./Input";
 import Button from "./Button";
 import { styled } from "@stitches/react";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { Todo } from "../type";
+import { todoAtomFamily, todosAtom } from "../store";
+import { useSetAtom } from "jotai";
 
 const Container = styled("div", {
   margin: "0 auto",
@@ -12,11 +17,35 @@ type AddTodoProps = {
   addTodo: () => void;
 };
 
-export default function AddTodo({ addTodo }: AddTodoProps) {
+export default function AddTodo() {
+  const [value, setValue] = useState("");
+  const setTodos = useSetAtom(todosAtom);
+
+  const addTodo = () => {
+    if (!value) return;
+    let newTodo: Todo = {
+      id: nanoid(),
+      todo: value,
+      isCompleted: false,
+      isUrgent: false,
+      reactions: [],
+    };
+
+    todoAtomFamily(newTodo);
+    setTodos((prev) => [...prev, newTodo]);
+    setValue("");
+  };
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
   return (
     <Container>
-      <Input />
-      <Button onClick={addTodo}>Add todo</Button>
+      <Input value={value} onChange={handleChange} />
+      <Button disabled={!Boolean(value)} onClick={addTodo}>
+        Add todo
+      </Button>
     </Container>
   );
 }
